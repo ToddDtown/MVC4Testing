7fec2221f095889cb63cddaca440eed31ef28cc6
Todd Jasper <todd.jasper@turn5.com>
2013-10-23 15:50:19 -0400

    Updated assembly information.  Added version number INI file.

--- Turn5/Turn5.Web.UI/Properties/AssemblyInfo.cs
+++ Turn5/Turn5.Web.UI/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Turn5.Web.UI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MVC4 Testing Application")]
+[assembly: AssemblyDescription("This application is used to test MVC4 techniques and build configurations.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Turn5, Inc.")]
 [assembly: AssemblyProduct("Turn5.Web.UI")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyCopyright("©Turn5, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- Turn5/Turn5.WebServices/Properties/AssemblyInfo.cs
+++ Turn5/Turn5.WebServices/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Turn5.WebServices")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MVC4 Testing Application")]
+[assembly: AssemblyDescription("This application is used to test MVC4 techniques and build configurations.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Turn5, Inc.")]
 [assembly: AssemblyProduct("Turn5.WebServices")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyCopyright("©Turn5, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
