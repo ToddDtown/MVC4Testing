caf5eb99accc4fe0849412b596fb29f4f285e8ed
Todd Jasper <todd.jasper@turn5.com>
2018-04-26 10:29:03 -0400

    Added some product model stuff

--- TestSite.Web.UI/Controllers/HomeController.cs
+++ TestSite.Web.UI/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestSite.Web.UI.Models;
 
 namespace TestSite.Web.UI.Controllers
 {
@@ -25,6 +26,39 @@ namespace TestSite.Web.UI.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult GetProducts()
+        {
+            var productGrid = new ProductGrid
+            {
+                Products = new List<ProductModel>
+                {
+                    new ProductModel
+                    {
+                        Id = 1,
+                        Name = "Raxiom Headlights",
+                        Description = "Raxiom headlights will change your life.  These headlights kick ass.",
+                        Price = new decimal(599.99)
+                    },
+                    new ProductModel
+                    {
+                        Id = 1,
+                        Name = "Catback Exhausts",
+                        Description = "Upgrade your 2011-2014 GT exhaust to a Pypes Pype-Bomb Super System Cat-Back Exhaust with Black Tips and yield an instant increase in horsepower at the rear wheels.",
+                        Price = new decimal(881.78)
+                    },
+                    new ProductModel
+                    {
+                        Id = 1,
+                        Name = "OPR Front Brake Caliper",
+                        Description = "Replace those old worn out or damaged front brake calipers with a set of re-manufactured OPR calipers to improve the stopping performance of your S197 Mustang.",
+                        Price = new decimal(128.99)
+                    }
+                }
+            };
+
+            return View("_Products", productGrid);
         }
     }
 }
---
+++ TestSite.Web.UI/Models/ProductGrid.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace TestSite.Web.UI.Models
+{
+    public class ProductGrid
+    {
+        public string Header { get; set; }
+        public List<ProductModel> Products { get; set; }
+    }
+}
